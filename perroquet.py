021b3319bc05f8e4612e7f376458ff6afc775c70
Mauceri <cmauceri@gmail.com>
2024-02-08 20:13:24 +0100

    perroquet

--- perroquet.py
+++ perroquet.py
@@ -19,18 +19,9 @@ class Echo(IObserver):
         return "!echo"
 
     
-class Ping(IObserver):
-    def __init__(self,observable:Callbacks=None):
-        self.observable =observable
 
-    async def notify(self,room:MatrixRoom, event:RoomMessageText, msg:str):
-        logger.info(f"***************************** L'utilisateur {event.sender} a écrit {msg} depuis ls salon {room.name}")
-        await self.observable.notify(room, event, f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}")
-
-    def prefix(self):
-        return "!ping"
     
-class Baba(IObserver):
+class Perroquet(IObserver):
     def __init__(self,observable:Callbacks=None):
         self.observable =observable
 
@@ -39,25 +30,17 @@ class Baba(IObserver):
         await self.observable.notify(room, event, f"L'utilisateur {event.sender} a écrit \"{msg}\" depuis le salon {room.name}")
 
     def prefix(self):
-        return "!baba"
+        return "!coco"
     
 class Plugin(IPlugin):
     def __init__(self,observable:IObservable):
         self.observable = observable
-        self.echo = Echo(observable)
-        logger.info(f"********************** Observateur créé {self.echo.prefix()}")
-        self.ping = Ping(observable)
-        logger.info(f"********************** Observateur créé {self.ping.prefix()}")
-        self.baba = Baba(observable)
-        logger.info(f"********************** Observateur créé {self.baba.prefix()}")
+        self.perroquet = Perroquet(observable)
+        logger.info(f"********************** Observateur créé {self.perroquet.prefix()}")
         
     def start(self):
-        logger.info(f"********************** Inscripton de {self.echo.prefix()}")
-        self.observable.subscribe(self.echo)
-        logger.info(f"********************** Inscripton de {self.ping.prefix()}")
-        self.observable.subscribe(self.ping)
-        logger.info(f"********************** Inscripton de {self.baba.prefix()}")
-        self.observable.subscribe(self.baba)
+        logger.info(f"********************** Inscripton de {self.perroquet.prefix()}")
+        self.observable.subscribe(self.perroquet)
 
     async def stop(self):
         pass
